00046d51c79484d3032ed9b6950238d1fb101ec7
kou003 <kou.000333@gmail.com>
2021-06-16 11:46:45 +0900

    fix

--- userscript/nh-rep.user.js
+++ userscript/nh-rep.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         nh-rep
 // @namespace    https://github.com/kou003/
-// @version      1.0
+// @version      1.1
 // @description  nh-rep
 // @author       kou003
 // @match        *://nhentai.net/g/*/*
@@ -17,9 +17,9 @@
     let url=`https://192.168.0.80:55649/${_gallery.id}/`;
     fetch(url,{method:'HEAD'}).then(r=>{
       if (r.ok) {
+        let t=document.querySelector("#image-container");
         let f=e=>t.querySelector('img').src=t.querySelector('img').src.replace(/.*?([^/]*$)/, (_,m)=>url+m);
         f();
-        let t=document.querySelector("#image-container");
         let observer = new MutationObserver(f);
         observer.observe(t,{ childList: true, subtree: true });
       }
