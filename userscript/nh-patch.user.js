9d2a289a26c613830bce401d0564b9b02229662c
kou003 <kou.000333@gmail.com>
2023-02-27 02:49:50 +0900

    fix

--- userscript/nh-patch.user.js
+++ userscript/nh-patch.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         nh-patch
 // @namespace    https://github.com/kou003/
-// @version      0.1.0
+// @version      0.1.1
 // @description  Address mysterious hyperlink bug 
 // @author       kou003
 // @match        *://nhentai.net/*
@@ -14,7 +14,7 @@
 {
   'use strict';
   const main = () => {
-    document.querySelectorAll('#cover>a, a.tag, a.cover').forEach(a => {
+    document.querySelectorAll('#cover>a, a.tag, a.cover, a.gallerythumb').forEach(a => {
       a.addEventListener('click', ({currentTarget: {href}}) => href && (location.href=href))
     });
   }
