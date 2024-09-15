fa0429513c9127bb9438f6e14b3c85425111fa43
kou003 <kou.000333@gmail.com>
2024-09-16 02:48:40 +0900

    Update

--- userscript/nh-troll.user.js
+++ userscript/nh-troll.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         nh-troll
 // @namespace    https://github.com/kou003/
-// @version      1.0.0
+// @version      1.0.1
 // @description  nh-troll
 // @author       kou003
 // @match        *://nhentai.net/*
@@ -14,24 +14,16 @@
 {
   'use strict';
 
-  const main = async () => {
-    document.querySelectorAll('img').forEach(img => {
-      img.dataset.tlist = '2 3 5 7';
-      img.addEventListener('error', e => {
-        const m = img.src.match(/^https:\/\/t(\d+).nhentai.net/);
-        if (m) {
-          const tlist = img.dataset.tlist.split(' ').filter(t => t != m[1]);
-          if (tlist.length == 0) return;
-          img.src = img.src.replace(`t${m[1]}.nhentai.net`, `t${tlist.pop()}.nhentai.net`);
-          img.dataset.tlist = tlist.join(' ');
-        }
-      });
-    });
-  }
-
-  if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', main);
-  } else {
-    main();
-  }
+  window.addEventListener('error', e => {
+    const img = e.target;
+    if (img.tagName != 'IMG') return;
+    const m = img.src.match(/^https:\/\/t(\d+).nhentai.net/);
+    if (m) {
+      img.dataset.tlist ??= '2 3 5 7';
+      const tlist = img.dataset.tlist?.split(' ').filter(t => t != m[1]);
+      if (tlist.length == 0) return;
+      img.src = img.src.replace(`t${m[1]}.nhentai.net`, `t${tlist.pop()}.nhentai.net`);
+      img.dataset.tlist = tlist.join(' ');
+    }
+  }, true);
 }
