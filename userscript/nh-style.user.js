abd8e7c63791c26fad3aeef15a38724a130e26ee
kou003 <kou.000333@gmail.com>
2024-10-22 03:38:36 +0900

    update

--- userscript/nh-style.user.js
+++ userscript/nh-style.user.js
@@ -1,14 +1,17 @@
 // ==UserScript==
 // @name         nh-style
 // @namespace    https://github.com/kou003/
-// @version      0.1.0
+// @version      0.1.1
 // @description  nh-style
 // @author       kou003
 // @match        *://nhentai.net/*
 // @updateURL    https://github.com/kou003/nhentai-util/raw/master/userscript/nh-style.user.js
 // @downloadURL  https://github.com/kou003/nhentai-util/raw/master/userscript/nh-style.user.js
 // @grant        none
-// @run-at       document-end
+// @run-at       document-start
 // ==/UserScript==
 
-document.head.insertAdjacentHTML('beforeend', `<style>.advertisement {display: none;}</style>`);
+{
+  'use strict';
+  document.head.insertAdjacentHTML('beforeend', `<style>.advertisement{display: none;}</style>`);
+}
