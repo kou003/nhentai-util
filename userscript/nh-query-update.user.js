81f599e6cbefe185b706e9085b9d6f0f99642bf3
kou003 <kou.000333@gmail.com>
2026-04-05 17:59:39 +0900

    update

--- userscript/nh-query-update.user.js
+++ userscript/nh-query-update.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         nh-query-update
 // @namespace    https://github.com/kou003/
-// @version      2.0.0
+// @version      2.1.0
 // @description  nh-query-update
 // @author       kou003
 // @match        *://nhentai.net/*
@@ -13,15 +13,22 @@
 
 {
   'use strict';
+  const getQuery = () => {
+    if (location.pathname === '/search') return new URLSearchParams(location.search).get('q');
+    if (/^\/(tag|artist|character|parody|group|language|category)\//.test(location.pathname)) {
+      const [_, key, value] = location.pathname.match(/\/(.*)\/(.*)\//);
+      return `${key}:"${value.replaceAll('-', ' ')}"`;
+    }
+    return null;
+  }
   const update = () => {
-    if (location.pathname !== '/search') return;
-    const q = new URLSearchParams(location.search).get('q');
+    const query = getQuery();
     const input = document.querySelector('input[type="search"]');
-    if (q && input && input.value !== q) input.value = q;
+    if (query && input && input.value !== query) input.value = query;
   }
   const main = () => {
     const title = document.head.querySelector('title');
-    new MutationObserver(update).observe(document, { childList: true, subtree: true });
+    new MutationObserver(update).observe(title, { childList: true });
     update();
   }
   if (document.readyState == 'loading') {
